040fb8f3bc3c9d9ba0f5f55fd9492a406e2ded80
Anish <anishtr4@gmail.com>
2024-10-18 12:29:04 +0530

    Final code adjustments for linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,23 +4,22 @@ from app import app, db
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+
     with app.app_context():
         db.create_all()
-    
+
     with app.test_client() as client:
         yield client
-    
+
     with app.app_context():
         db.drop_all()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def init_database(test_client):
     db.create_all()
     yield
     db.session.remove()
     db.drop_all()
-    
